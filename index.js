b48eac615964ba92cd826bcc1c8708928070cfd7
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-08-13 18:56:02 +0100

    Updated code

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/student", (req, res) => {
     const query = `SELECT * FROM thecurveafrica.studentTable`;
     sql.query(query, (err, result) => {
       if (err) {
-        return res.json(400).json({ message: err.sqlMessage });
+        return res.status(400).json({ message: err.sqlMessage });
       } else {
         res.status(200).json({
           message: "Kindly find below all users",
@@ -67,9 +67,9 @@ app.get("/student", (req, res) => {
 app.get("/student/:userinfo", (req, res) => {
   const { userinfo } = req.params;
 
-  const query = `SELECT * FROM thecurveafrica.studentTable WHERE id = ${userinfo}`;
+  const query = `SELECT * FROM thecurveafrica.studentTable WHERE id = ?`;
 
-  sql.query(query, (err, result) => {
+  sql.query(query, [userinfo], (err, result) => {
     if (err) {
       res.status(400).json({
         message: err.sqlMessage,
@@ -86,11 +86,11 @@ app.get("/student/:userinfo", (req, res) => {
 // Update
 app.put("/student/:id", (req, res) => {
   try {
-    const query = `UPDATE thecurveafrica.studentTable SET fullName = ?, email = ?, address = ? WHERE id = ${req.params.id}
+    const query = `UPDATE thecurveafrica.studentTable SET fullName = ?, email = ?, address = ? WHERE id = ?
     `;
     sql.query(
       query,
-      [req.body.fullName, req.body.email, req.body.address],
+      [req.body.fullName, req.body.email, req.body.address, req.params.id],
       (err) => {
         if (err) {
           return res.status(400).json({ message: err.message });
@@ -108,6 +108,7 @@ app.put("/student/:id", (req, res) => {
 });
 
 //DELETE
+//Delete one student
 app.delete("/student/:id", (req, res) => {
   try {
     const { id } = req.params;
@@ -128,6 +129,81 @@ app.delete("/student/:id", (req, res) => {
   } catch (err) {
     res.status(500).json({
       message: "Internal server error",
+      error: err.message,
+    });
+  }
+});
+
+//CREATE Student Scores
+app.post("/score/:studentid", (req, res) => {
+  try {
+    const query = `INSERT INTO thecurveafrica.studentScore (userid, punctuality, assignment, totalScore) VALUES (?, ?, ?, ?)`;
+    const { punctuality, assignment } = req.body;
+    const totalScore = punctuality + assignment;
+    const { studentid } = req.params;
+    sql.query(
+      query,
+      [studentid, punctuality, assignment, totalScore],
+      (err) => {
+        if (err) {
+          return res.status(400).json({
+            message: "Something went wrong",
+            error: err.sqlMessage,
+          });
+        }
+        res.status(200).json({
+          message: "Student score successfully created",
+        });
+      }
+    );
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error",
+      error: err.message,
+    });
+  }
+});
+
+//GET All student Scores
+app.get("/scores", (req, res) => {
+  try {
+    const query = `SELECT p.fullName AS mystudentName, p.stack, b.punctuality, b.assignment, b.totalScore FROM thecurveafrica.studentTable p JOIN thecurveafrica.studentScore b ON p.id= b.userid`;
+    sql.query(query, (err, result) => {
+      if (err) {
+        return res.status(400).json({ err: err.sqlMessage });
+      }
+      res
+        .status(200)
+        .json({ message: "Kindly find below, student info", data: result });
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error",
+      error: err.message,
+    });
+  }
+});
+
+//Get one student score
+app.get("/scores/:id", (req, res) => {
+  try {
+    const query = `SELECT p.fullName AS mystudentName, p.stack, b.punctuality, b.assignment, b.totalScore FROM thecurveafrica.studentTable p JOIN thecurveafrica.studentScore b ON p.id= b.userid WHERE p.id = ${req.params.id}`;
+    sql.query(query, (err, result) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ message: "No student found", err: err.sqlMessage });
+      }
+      if (result.length === 0) {
+        return res.status(404).json({ message: "No score found", error: err });
+      }
+      res
+        .status(200)
+        .json({ message: "Kindly find below, student info", data: result });
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error",
       error: err.message,
     });
   }
